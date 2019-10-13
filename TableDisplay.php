545d1ba6a339b4d36dac93e4d706b55ad5f2d05e
志摩圭太 <shimakeita@mbp.local>
2019-10-13 17:50:55 +0900

    fix tableDisplay

--- TableDisplay.php
+++ TableDisplay.php
@@ -1,3 +1,36 @@
 <?php 
+require_once 'AbstractDisplay.php';
+?>
 
+<?php
+/**
+ * ConcreteClass
+ */
+class TableDisplay extends AbstractDisplay {
+    /**
+     * ヘッダを表示
+     */
+    protected function displayHeader() {
+        echo '<table border="1" cellpadding="2" cellspacing="2">';
+    }
+
+    /**
+     * ボディを表示
+     */
+    protected function displayBody() {
+        foreach ($this->getData() as $key => $value) {
+            echo '<tr>';
+            echo '<th>' . $key . '</th>';
+            echo '<td>' . $value . '</td>';
+            echo '</tr>';
+        }
+    }
+
+    /**
+     * フッタを表示
+     */
+    protected function displayFooter() {
+        echo '</table>';
+    }
+}
 ?>
