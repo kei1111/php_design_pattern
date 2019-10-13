50635ed2c5d93bffc1b3d210607202cd593abfac
志摩圭太 <shimakeita@mbp.local>
2019-10-13 17:34:05 +0900

    #abstract fix

--- AbstractDisplay.php
+++ AbstractDisplay.php
@@ -14,6 +14,40 @@ abstract class AbstractDisplay {
         }
         $this->data = $data;
     }
+
+    /**
+     * template method
+     */
+    public function display() {
+        $this->displayHeader();
+        $this->displayBody();
+        $this->displayFooter();
+    }
+
+    /**
+     * データ取得
+     */
+    public function getData() {
+        return $this->data;
+    }
+
+    /**
+     * ヘッダ
+     * サブクラス実装用抽象メソッド
+     */
+    protected abstract function displayHeader();
+
+    /**
+     * ボディを表示
+     * サブクラス実装用抽象メソッド
+     */
+    protected abstract function displayBody();
+
+    /**
+     * フッタを表示
+     * サブクラス実装用抽象メソッド
+     */
+    protected abstract function displayFooter();
 }
 
 
