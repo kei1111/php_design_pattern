8edcf66ce0ee9896368cd3a43172e6069a4e3dde
kei1111 <ke1taki6a25@outlook.jp>
2020-02-23 00:36:50 +0900

    salesmaniterater get

---
+++ Iterator/SalesmanIterator.class.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'Employee.class.php';
+?>
+<?php
+class SalesmanIterator extends FilterIterator {
+    public function __construct($iterator) {
+        parent::__construct($iterator);
+    }
+    public function accept() {
+        $employee = $this->current();
+        return ($employee->getJob() === 'SALESMAN');
+    }
+}
+?>
